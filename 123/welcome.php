22dbf9591267c93b1c1f9e678c50c7d3a254f3cc
bnghnb <tungxeko0608@gmail.com>
2024-05-22 08:07:59 +0700

    ToNi 3

--- 123/welcome.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Kiểm tra xem người dùng đã đăng nhập hay chưa
-if (!isset($_SESSION["username"])) {
-  header("Location: index.html");
-  exit();
-}
-
-echo "Chào mừng " . $_SESSION["username"] . "!";
-?>
