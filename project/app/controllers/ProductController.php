e0ae6c46f52682703c3904b548c43490ea702708
HP_Nam <namphg1412@gmail.com>
2024-06-05 07:29:29 +0700

    del old files

--- project/app/controllers/ProductController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ProductController extends Controller {
-    public function index() {
-        $product = $this->model('Product');
-        $data['products'] = $product->getAll();
-        $this->view('product/index', $data);
-    }
-}
-?>
